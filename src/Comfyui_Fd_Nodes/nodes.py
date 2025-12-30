30dfe8dc60a900c7e6c861dc0bb7200e067b5034
xuyucai <xuyucai@zhiyitech.cn>
2025-12-30 10:59:43 +0000

    Delete Example Node, it's only for example

--- src/Comfyui_Fd_Nodes/nodes.py
+++ src/Comfyui_Fd_Nodes/nodes.py
@@ -252,11 +252,7 @@ class Example:
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
-NODE_CLASS_MAPPINGS = {
-    "Example": Example
-}
+NODE_CLASS_MAPPINGS = {"FD_RemoveWatermark": FD_RemoveWatermark}
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "Example": "Example Node"
-}
+NODE_DISPLAY_NAME_MAPPINGS = {"FD_RemoveWatermark": "Remove Watermark"}
