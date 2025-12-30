ab551500fb4129cb189405b42d88ff509efcf6c3
xuyucai <xuyucai@zhiyitech.cn>
2025-12-30 09:39:30 +0000

    Init.

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for Comfyui_Fd_Nodes."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """xuyucai"""
+__email__ = "986327386@qq.com"
+__version__ = "0.0.1"
+
+from .src.Comfyui_Fd_Nodes.nodes import NODE_CLASS_MAPPINGS
+from .src.Comfyui_Fd_Nodes.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for Comfyui_Fd_Nodes."""
